b8f16e222f1919788e6659d148d1f3897adb2bfb
Alexnortung <alex_nortung@live.dk>
2019-04-04 21:07:09 +0200

    added jsdoc to item

--- item.js
+++ item.js
@@ -1,9 +1,18 @@
 class Item {
+	/**
+	 * 
+	 * @param {*} value the value of the current item
+	 * @param {Subgroup[]} acceptingSubgroups Subgroubs which accept this item
+	 */
 	constructor(value, acceptingSubgroups) {
 		this.value = value;
 		this.acceptingSubgroups = acceptingSubgroups || [];
 	}
 
+	/**
+	 * 
+	 * @param {Subgroup[]} subgroups setting new subgroups if they accept this items value
+	 */
 	setAcceptingSubgroups(subgroups) {
 		this.acceptingSubgroups = [];
 		for (var i = 0; i < subgroups.length; i++) {
