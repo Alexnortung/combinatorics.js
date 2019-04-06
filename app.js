212fcda77f232468d0714853532abe6b0a697be9
Alexnortung <alex_nortung@live.dk>
2019-04-06 15:49:47 +0200

    added semicolon

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const uniqueOrderer = require('./uniqueOrderer.js');
-const orderer = require("./orderer.js")
+const orderer = require("./orderer.js");
 
 module.exports.permutation = uniqueOrderer;
 module.exports.permutationUnfiltered = orderer;
